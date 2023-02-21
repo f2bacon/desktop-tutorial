16796f32fac9528bdb97db4e99f8a4cd735429ae
f2bacon <f2bacon@gmail.com>
2023-02-21 08:47:37 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ var profilesKey = 'darksouls3_profiles';
     'use strict';
 
     var themes = {
+		"Darkly" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/darkly/bootstrap.min.css",
+        "Flatly" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/flatly/bootstrap.min.css",
         "Standard" : "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
         "Cosmo" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cosmo/bootstrap.min.css",
         "Cyborg" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cyborg/bootstrap.min.css",
-        "Darkly" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/darkly/bootstrap.min.css",
-        "Flatly" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/flatly/bootstrap.min.css",
         "Journal" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/journal/bootstrap.min.css",
         "Lumen" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/lumen/bootstrap.min.css",
         "Paper" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/paper/bootstrap.min.css",
@@ -366,7 +366,7 @@ var profilesKey = 'darksouls3_profiles';
     // Setup ("bootstrap", haha) styling
     function themeSetup(stylesheet) {
         if(stylesheet === null || stylesheet === undefined) { // if we didn't get a param, then
-            stylesheet = $.jStorage.get("style") || "Standard"; // fall back on "light" if cookie not set
+            stylesheet = $.jStorage.get("style") || "darkly"; // fall back on "light" if cookie not set
         }
         $("#bootstrap").attr("href", themes[stylesheet]);
     }
